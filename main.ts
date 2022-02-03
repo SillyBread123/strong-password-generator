b62a72c72f8379f160fcfa0361a80af4c5f7a27d
SillyBread123 <98095876+SillyBread123@users.noreply.github.com>
2022-02-03 11:59:34 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    number = randint(10, 99)
+    basic.showNumber(number)
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        word = randint(1, 12)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+let word = 0
+let number = 0
+basic.showString("Press A or B")
